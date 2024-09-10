e2fea459cbc54a2c44bc883961569b1dc962a1ea
Goodmorningpeople <zachy2020p@gmail.com>
2024-09-10 18:05:13 +0800

    Switched from pat router to chi router and implemented Panic Recovery middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -5,14 +5,17 @@ import (
 
 	"github.com/Goodmorningpeople/learning_web_with_go/pkg/config"
 	"github.com/Goodmorningpeople/learning_web_with_go/pkg/handlers"
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
+// routes func contains all the routes, which are served to an http server in main.go (using chi router)
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+	mux := chi.NewRouter()
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux.Use(middleware.Recoverer)
 
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 	return mux
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/Goodmorningpeople/learning_web_with_go
 
 go 1.23.0
 
-require github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f // indirect
+require github.com/go-chi/chi/v5 v5.1.0
